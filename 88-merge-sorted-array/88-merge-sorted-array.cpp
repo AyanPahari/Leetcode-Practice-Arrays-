32037bf1afe0cea90ae526ee9c01c764ed045ab5
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-01-21 01:08:12 +0530

    Time: 4 ms (51.99%), Space: 9.3 MB (28.83%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -5,16 +5,10 @@ public:
         int i=0,j=0;
         while(i<m && j<n){
             if(nums1[i]<=nums2[j]) temp.push_back(nums1[i++]);
-            else{
-                temp.push_back(nums2[j++]);
-            }
+            else temp.push_back(nums2[j++]);
         }
-        while(i<m){
-            temp.push_back(nums1[i++]);
-        }
-        while(j<n){
-            temp.push_back(nums2[j++]);
-        }
+        while(i<m) temp.push_back(nums1[i++]);
+        while(j<n) temp.push_back(nums2[j++]);
         nums1=temp;
     }
 };
