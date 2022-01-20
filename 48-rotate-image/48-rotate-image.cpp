f709d457f6b448ebf954af765cd6a9239ac61e70
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-01-20 23:50:15 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (32.47%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        vector<vector<int>>copy=matrix;
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                copy[i][j]=matrix[n-j-1][i];
+            }
+        }
+        matrix=copy;
+    }
+};
